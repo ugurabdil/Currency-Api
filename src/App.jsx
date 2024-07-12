c6909a99f18e1364116d27b1c241acde971c46b8
ugurabdil98 <ugurabdil98@gmail.com>
2024-07-12 18:55:19 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import Currency from './components/Currency'
+function App() {
+   
+
+  return (
+    <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+      <Currency/>
+    </div>
+  )
+}
+
+export default App
